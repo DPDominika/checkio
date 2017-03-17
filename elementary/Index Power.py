3aa8cdac272d04db023b58562234c457ba586fb4
Dominika Plesniak <dominika.plesniak@gmail.com>
2017-03-17 10:32:02 +0100

    Exercises - Checkio, first commit

---
+++ elementary/Index Power.py
@@ -0,0 +1,10 @@
+def index_power(array, n):
+    """
+        Find Nth power of the element with index N.
+    """
+    if n == 0:
+        return 1
+    elif len(array)-1 < n:
+        return -1
+    elif len(array)-1 >= n:
+        return (array[n])**n
